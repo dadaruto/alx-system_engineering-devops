fa198b54d0978ea3c99c48513b2390c8c922e6f0
dadaruto <cheladorcas.ruto@gmail.com>
2023-10-04 13:42:03 -0700

    networking basic

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,21 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/School/).join
 
+# Check if there is exactly one command-line argument
+if ARGV.length != 1
+  puts "Usage: #{$0} <input_string>"
+  exit 1
+end
+
+# Extract the input string from the command-line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern
+pattern = /School/
+
+# Check if the input string matches the pattern
+if input_string.match?(pattern)
+  puts input_string
+else
+  # If there is no match, print an empty line
+  puts ""
+end
