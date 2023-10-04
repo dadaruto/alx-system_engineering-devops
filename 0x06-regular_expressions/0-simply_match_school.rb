09656471fcfc472cae48a36f9978b480d4242af2
dadaruto <cheladorcas.ruto@gmail.com>
2023-10-04 06:41:14 -0700

    regex

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,15 @@
 #!/usr/bin/env ruby
 
-puts ARGV[0].scan(/School/).join
+def match_school(input)
+  regex = /\bSchool\b/ 
+  if input.match?(regex)
+    puts "#{input}$"
+  else
+    puts '$'
+  end
+end
+
+
+input = ARGV[0]
+
+match_school(input)
